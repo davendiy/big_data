a280510854b12529683e229de11598bb8ed8c510
davendiy <davendiy@gmail.com>
2020-02-25 09:58:24 +0200

    hw 2, task0: tsne, umap, pca

---
+++ test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Excusa. Quod scripsi, scripsi.
+
+# @Author: David Zashkolny <davendiy>
+# @Date:   18-Feb-2020
+# @Email:  davendiy@gmail.com
+# @Last modified by:   davendiy
+# @Last modified time: 18-Feb-2020
---
+++ test2.py
@@ -0,0 +1,5 @@
+# @Author: David Zashkolny <davendiy>
+# @Date:   18-Feb-2020
+# @Email:  davendiy@gmail.com
+# @Last modified by:   davendiy
+# @Last modified time: 18-Feb-2020
